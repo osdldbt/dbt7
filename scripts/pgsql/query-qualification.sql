57186add1da789e070075b8564d559c7efd38ef6
Mark Wong <markwkm@gmail.com>
2025-02-10 13:03:09 -0800

    pgsql: A couple of corrections to query qualification script
    These might be mistakes in the script, as opposed to updates in the
    v4.0.0 spec.

--- scripts/pgsql/query-qualification.sql
+++ scripts/pgsql/query-qualification.sql
@@ -3,11 +3,11 @@
 with customer_total_return as
 (select sr_customer_sk as ctr_customer_sk
 ,sr_store_sk as ctr_store_sk
-,sum(SR_FEE) as ctr_total_return
+,sum(SR_RETURN_AMT) as ctr_total_return
 from store_returns
 ,date_dim
 where sr_returned_date_sk = d_date_sk
-and d_year =2002
+and d_year = 2000
 group by sr_customer_sk
 ,sr_store_sk)
  select  c_customer_id
